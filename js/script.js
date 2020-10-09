6e1c2232c0f8122c53103f1b321ddcd6f72a4888
NicolineRusbak <57627700+NicolineRusbak@users.noreply.github.com>
2020-10-09 15:03:45 +0200

    upload portfolie

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//funktione´der fjerner webapps
+function function1() {
+    let umf = document.getElementById("umf");
+    if (umf.style.display === "none") {
+      umf.style.display = "block";
+    } else {
+      umf.style.display = "none";
+    }
+}
+//funktione´der fjerner websitets
+function function2() {
+    let df = document.getElementById("df");
+    if (df.style.display === "none") {
+      df.style.display = "block";
+    } else {
+      df.style.display = "none";
+    }
+}
